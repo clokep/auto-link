abf17d885202349002f66360b346097e7c9be86a
Patrick Cloke <clokep@gmail.com>
2010-07-09 16:23:25 -0400

    Moving things around and merging files

--- chrome/content/options.js
+++ chrome/content/options.js
@@ -68,3 +68,25 @@ function closePopup(event) {
 	}
 	treeCell.value = JSON.stringify(ruleProtocols);
 }
+
+function parse() {
+	var regex = document.getElementById("regex"),
+			text = regex.value,
+			regex_bg = document.getElementById("regex_bg");
+	
+	// From http://jszen.blogspot.com/2007/02/how-to-parse-html-strings-into-dom.html
+	var range = document.createRange();
+	range.selectNode(regex);
+	var doc = range.createContextualFragment("<span class=\"regex\">" + highlightJsReSyntax(text) + "</span>");
+
+	// Remove all current children
+	while (regex_bg.childNodes.length >= 1 )
+		regex_bg.removeChild(regex_bg.firstChild);       
+
+	// Replace with new children
+	for (var i = 0; i < doc.childNodes.length; i++) {
+		regex_bg.appendChild(doc.childNodes[i]);
+	}
+}
+
+window.addEventListener("load", function(e) { parse(); }, false);
