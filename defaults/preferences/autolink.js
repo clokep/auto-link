e3735d1698a91aaf3d7a8d25d997dbbee8e287cd
Patrick Cloke <clokep@gmail.com>
2010-07-09 08:46:24 -0400

    Default preferences Update Update version

--- defaults/preferences/autolink.js
+++ defaults/preferences/autolink.js
@@ -6,7 +6,7 @@
 						"title" : "Bug $1 @ bugzilla.mozilla.org",
 						"protocols" : ["prpl-irc"],
 						"users" : ["clokep","clokep_work"],
-						"rooms" : ["#[^(instant|song)bird]"]
+						"rooms" : ["#[^(instant|song)bird]"] // Possibly remove this constraint and just run the other two first since they're more "specific"
 					},
 					{
 						"pattern" : "bug (\\d+)",
@@ -18,31 +18,13 @@
 						"rooms" : ["#instantbird"]
 					},
 					{
-						"pattern" : "AIO",
-						"flags" : "gi",
-						"link" : "http://addons.instantbird.org/",
-						"title" : "addons.instantbird.org",
-						"protocols" : ["prpl-irc"],
-						"users" : [".+"],
-						"rooms" : ["#instantbird"]
-					},
-					{
-						"pattern" : "AIO",
-						"flags" : "gi",
-						"link" : "http://addons.instantbird.org/",
-						"title" : "addons.instantbird.org",
-						"protocols" : ["prpl-irc"],
-						"users" : [".+"],
-						"rooms" : ["#instantbird"]
-					},
-					{
 						"pattern" : "bug (\\d+)",
 						"flags" : "gi",
-						"link" : "https://bugzilla.songbird.org/show_bug.cgi?id=$1",
-						"title" : "Bug $1 @ bugzilla.songbird.org",
+						"link" : "http://bugzilla.songbirdnest.com/show_bug.cgi?id=id=$1",
+						"title" : "Bug $1 @ bugzilla.songbirdnest.com",
 						"protocols" : ["prpl-irc"],
 						"users" : [".+"],
 						"rooms" : ["#songbird"]
 					}
 				]).replace(/\\/g,"\\\\")*/
-pref("extensions.autolink.rules",'[{"pattern":"bug (\\\\d+)","flags":"gi","link":"https://bugzilla.mozilla.org/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.mozilla.org","protocols":["prpl-irc"],"users":["clokep","clokep_work"],"rooms":["#[^(instant|song)bird]"]},{"pattern":"bug (\\\\d+)","flags":"gi","link":"https://bugzilla.instantbird.org/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.instantbird.org","protocols":["prpl-irc"],"users":[".+"],"rooms":["#instantbird"]},{"pattern":"AIO","flags":"gi","link":"http://addons.instantbird.org/","title":"addons.instantbird.org","protocols":["prpl-irc"],"users":[".+"],"rooms":["#instantbird"]},{"pattern":"AIO","flags":"gi","link":"http://addons.instantbird.org/","title":"addons.instantbird.org","protocols":["prpl-irc"],"users":[".+"],"rooms":["#instantbird"]},{"pattern":"bug (\\\\d+)","flags":"gi","link":"https://bugzilla.songbird.org/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.songbird.org","protocols":["prpl-irc"],"users":[".+"],"rooms":["#songbird"]}]');
+pref("extensions.autolink.rules",'[{"pattern":"bug (\\\\d+)","flags":"gi","link":"https://bugzilla.mozilla.org/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.mozilla.org","protocols":["prpl-irc"],"users":["clokep","clokep_work"],"rooms":["#[^(instant|song)bird]"]},{"pattern":"bug (\\\\d+)","flags":"gi","link":"https://bugzilla.instantbird.org/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.instantbird.org","protocols":["prpl-irc"],"users":[".+"],"rooms":["#instantbird"]},{"pattern":"bug (\\\\d+)","flags":"gi","link":"http://bugzilla.songbirdnest.com/show_bug.cgi?id=id=$1","title":"Bug $1 @ bugzilla.songbirdnest.com","protocols":["prpl-irc"],"users":[".+"],"rooms":["#songbird"]}]');
