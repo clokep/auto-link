570228a27de40321213d61c933cdc49f3e37e8f0
Patrick Cloke <clokep@gmail.com>
2010-07-10 19:06:21 -0400

    Working (and checked!) for BMO

--- defaults/preferences/autolink.js
+++ defaults/preferences/autolink.js
@@ -6,7 +6,7 @@
 						"title" : "Bug $1 @ bugzilla.mozilla.org",
 						"protocols" : ["prpl-irc"],
 						"users" : [".+"],
-						"rooms" : ["#[^(instant|song)bird]"] // Possibly remove this constraint and just run the other two first since they're more "specific"
+						"rooms" : ["#(?!(instant|song)bird).+"] // Possibly remove this constraint and just run the other two first since they're more "specific"
 					},
 					{
 						"pattern" : "bug (\\d+)",
@@ -27,4 +27,4 @@
 						"rooms" : ["#songbird"]
 					}
 				]).replace(/\\/g,"\\\\")*/
-pref("extensions.autolink.rules",'[{"pattern":"bug (\\\\d+)","flags":"gi","link":"https://bugzilla.mozilla.org/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.mozilla.org","protocols":["prpl-irc"],"users":[".+"],"rooms":["#[^(instant|song)bird]"]},{"pattern":"bug (\\\\d+)","flags":"gi","link":"https://bugzilla.instantbird.org/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.instantbird.org","protocols":["prpl-irc"],"users":[".+"],"rooms":["#instantbird"]},{"pattern":"bug (\\\\d+)","flags":"gi","link":"http://bugzilla.songbirdnest.com/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.songbirdnest.com","protocols":["prpl-irc"],"users":[".+"],"rooms":["#songbird"]}]');
+pref("extensions.autolink.rules",'[{"pattern":"bug (\\\\d+)","flags":"gi","link":"https://bugzilla.mozilla.org/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.mozilla.org","protocols":["prpl-irc"],"users":[".+"],"rooms":["#(?!(instant|song)bird).+"]},{"pattern":"bug (\\\\d+)","flags":"gi","link":"https://bugzilla.instantbird.org/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.instantbird.org","protocols":["prpl-irc"],"users":[".+"],"rooms":["#instantbird"]},{"pattern":"bug (\\\\d+)","flags":"gi","link":"http://bugzilla.songbirdnest.com/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.songbirdnest.com","protocols":["prpl-irc"],"users":[".+"],"rooms":["#songbird"]}]');
