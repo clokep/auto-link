97c5ad1979fc6040466b5aed3e260e2437bb5414
Patrick Cloke <clokep@gmail.com>
2010-07-26 09:10:40 -0400

    Fix default prefs (users/rooms are no longer multiple items)

--- defaults/preferences/autolink.js
+++ defaults/preferences/autolink.js
@@ -4,24 +4,24 @@
 						"link" : "https://bugzilla.mozilla.org/show_bug.cgi?id=$1",
 						"title" : "Bug $1 @ bugzilla.mozilla.org",
 						"protocols" : ["prpl-irc"],
-						"users" : [".+"],
-						"rooms" : ["#(?!(instant|song)bird).+"] // Possibly remove this constraint and just run the other two first since they're more "specific"
+						"users" : ".+",
+						"rooms" : "#(?!(instant|song)bird).+" // Possibly remove this constraint and just run the other two first since they're more "specific"
 					},
 					{
 						"rule" : /bug[ #]*?(\d+)/gi.toString(),
 						"link" : "https://bugzilla.instantbird.org/show_bug.cgi?id=$1",
 						"title" : "Bug $1 @ bugzilla.instantbird.org",
 						"protocols" : ["prpl-irc"],
-						"users" : [".+"],
-						"rooms" : ["#instantbird"]
+						"users" : ".+",
+						"rooms" : "#instantbird"
 					},
 					{
 						"rule" : /bug[ #]*?(\d+)/gi.toString(),
 						"link" : "http://bugzilla.songbirdnest.com/show_bug.cgi?id=$1",
 						"title" : "Bug $1 @ bugzilla.songbirdnest.com",
 						"protocols" : ["prpl-irc"],
-						"users" : [".+"],
-						"rooms" : ["#songbird"]
+						"users" : ".+",
+						"rooms" : "#songbird"
 					}
 				])*/
-pref("extensions.autolink.rules",'[{"rule":"/bug[ #]*?(\\d+)/gi","link":"https://bugzilla.mozilla.org/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.mozilla.org","protocols":["prpl-irc"],"users":[".+"],"rooms":["#(?!(instant|song)bird).+"]},{"rule":"/bug[ #]*?(\\d+)/gi","link":"https://bugzilla.instantbird.org/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.instantbird.org","protocols":["prpl-irc"],"users":[".+"],"rooms":["#instantbird"]},{"rule":"/bug[ #]*?(\\d+)/gi","link":"http://bugzilla.songbirdnest.com/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.songbirdnest.com","protocols":["prpl-irc"],"users":[".+"],"rooms":["#songbird"]}]');
+pref("extensions.autolink.rules",'[{"rule":"/bug[ #]*?(\\d+)/gi","link":"https://bugzilla.mozilla.org/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.mozilla.org","protocols":["prpl-irc"],"users":".+","rooms":"#(?!(instant|song)bird).+"},{"rule":"/bug[ #]*?(\\d+)/gi","link":"https://bugzilla.instantbird.org/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.instantbird.org","protocols":["prpl-irc"],"users":".+","rooms":"#instantbird"},{"rule":"/bug[ #]*?(\\d+)/gi","link":"http://bugzilla.songbirdnest.com/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.songbirdnest.com","protocols":["prpl-irc"],"users":".+","rooms":"#songbird"}]');
