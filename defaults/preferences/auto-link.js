4194b998d66d584e9bbbfcb124d777540de6e682
Patrick Cloke <clokep@gmail.com>
2010-07-26 12:42:32 -0400

    Fix prefs

--- defaults/preferences/auto-link.js
+++ defaults/preferences/auto-link.js
@@ -5,7 +5,7 @@
 						"title" : "Bug $1 @ bugzilla.mozilla.org",
 						"protocols" : ["prpl-irc"],
 						"accountName" : ".+",
-						"conversationNames" : "#(?!(instant|song)bird).+" // Possibly remove this constraint and just run the other two first since they're more "specific"
+						"conversationName" : /#(?!(instant|song)bird).+/i.toString() // Possibly remove this constraint and just run the other two first since they're more "specific"
 					},
 					{
 						"pattern" : /bug[ #]*?(\d+)/gi.toString(),
@@ -13,7 +13,7 @@
 						"title" : "Bug $1 @ bugzilla.instantbird.org",
 						"protocols" : ["prpl-irc"],
 						"accountName" : ".+",
-						"conversationNames" : "#instantbird"
+						"conversationName" : /#instantbird/i.toString()
 					},
 					{
 						"pattern" : /bug[ #]*?(\d+)/gi.toString(),
@@ -21,7 +21,7 @@
 						"title" : "Bug $1 @ bugzilla.songbirdnest.com",
 						"protocols" : ["prpl-irc"],
 						"accountName" : ".+",
-						"conversationNames" : "#songbird"
+						"conversationName" : /#songbird/i.toString()
 					}
-				])*/
-pref("extensions.autolink.rules",'[{"pattern":"/bug[ #]*?(\\d+)/gi","link":"https://bugzilla.mozilla.org/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.mozilla.org","protocols":["prpl-irc"],"accountName":".+","conversationNames":"#(?!(instant|song)bird).+"},{"pattern":"/bug[ #]*?(\\d+)/gi","link":"https://bugzilla.instantbird.org/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.instantbird.org","protocols":["prpl-irc"],"accountName":".+","conversationNames":"#instantbird"},{"pattern":"/bug[ #]*?(\\d+)/gi","link":"http://bugzilla.songbirdnest.com/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.songbirdnest.com","protocols":["prpl-irc"],"accountName":".+","conversationNames":"#songbird"}]');
+				]).replace(/\\/g, "\\\\")*/
+pref("extensions.autolink.rules",'[{"pattern":"/bug[ #]*?(\\\\d+)/gi","link":"https://bugzilla.mozilla.org/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.mozilla.org","protocols":["prpl-irc"],"accountName":".+","conversationName":"/#(?!(instant|song)bird).+/i"},{"pattern":"/bug[ #]*?(\\\\d+)/gi","link":"https://bugzilla.instantbird.org/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.instantbird.org","protocols":["prpl-irc"],"accountName":".+","conversationName":"/#instantbird/i"},{"pattern":"/bug[ #]*?(\\\\d+)/gi","link":"http://bugzilla.songbirdnest.com/show_bug.cgi?id=$1","title":"Bug $1 @ bugzilla.songbirdnest.com","protocols":["prpl-irc"],"accountName":".+","conversationName":"/#songbird/i"}]');
